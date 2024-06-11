4fb18c6f87b4f9f522c4b06d53d6d4b17791d276
Nilesh Srivastava <Nileshsri2022>
2024-06-11 12:21:50 +0530

    step-11

--- lib/constants/routes.dart
+++ lib/constants/routes.dart
@@ -2,3 +2,20 @@ const loginRoute = '/login/';
 const registerRoute = '/register/';
 const notesRoute = '/notes/';
 const verifyEmailRoute = '/verify-email/';
+const createUserTable = '''
+CREATE TABLE  IF NOT EXISTS"user" (
+	"id"	INTEGER NOT NULL,
+	"email"	TEXT NOT NULL UNIQUE,
+	PRIMARY KEY("id" AUTOINCREMENT)
+);
+''';
+const createNoteTable='''
+CREATE TABLE IF NOT EXISTS "note" (
+	"id"	INTEGER NOT NULL,
+	"user_id"	INTEGER NOT NULL UNIQUE,
+	"text"	TEXT,
+	"is_sync_with_cloud"	INTEGER NOT NULL DEFAULT 0,
+	FOREIGN KEY("user_id") REFERENCES "user"("id"),
+	PRIMARY KEY("id" AUTOINCREMENT)
+);
+''';
